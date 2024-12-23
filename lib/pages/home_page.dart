2d5201776701ff47a386743deffbaefa945fcfa5
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2024-12-23 17:35:33 +0100

    modified: created themes and button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:darklightmodeexperiment/components/box.dart';
+import 'package:darklightmodeexperiment/components/button.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +13,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[500],
-    );
+        backgroundColor: Colors.deepPurple[500],
+        body: Center(
+          child: MyBox(
+            color: Colors.deepPurple[300],
+            child: MyButton(
+              color: Colors.deepPurple[200],
+              onTap:(){}
+            ),
+          ),
+        ));
   }
 }
