00f95fac4e25c789c71dd14452c6b54c8ff605f6
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2024-12-24 09:05:43 +0100

    modified: was missing gesture detector in button widget.

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -12,14 +12,17 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        padding: const EdgeInsets.all(25),
-        child: const Center(
-          child: Text("TAP"),
-        ));
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: const EdgeInsets.all(25),
+          child: const Center(
+            child: Text("TAP"),
+          )),
+    );
   }
 }
